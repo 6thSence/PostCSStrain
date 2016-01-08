b851c73e63e045e727494a785bd4cead25d7d9d4
6thSence <d.pushkarskaya.pr@gmail.com>
2016-01-08 21:51:59 +0600

    PosthCSS with Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require('gulp');
 var postcss =require('gulp-postcss');
 var csswring = require('csswring');
-var stylus = require('gulp-stylus');
+var autoprefixer = require('autoprefixer-core');
 
 gulp.task('styles', function() {
   var processors = [
-    csswring
+    csswring,
+    autoprefixer({browsers:['last 2 version']})
   ];
-  return gulp.src('styles.styl')
-    .pipe(stylus())
+  return gulp.src('styles.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest('./dest'));
 });
 
 gulp.task('watch:styles', function(){
-  gulp.watch('**/*.styl', ['styles']);
+  gulp.watch('**/*.css', ['styles']);
 })
