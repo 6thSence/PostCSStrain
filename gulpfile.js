24eebe73574a489aa06bc746d567cc5ba0cc3c0c
6thSence <d.pushkarskaya.pr@gmail.com>
2016-01-09 13:44:18 +0600

    PostCSS with precss

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp');
 var postcss =require('gulp-postcss');
 var csswring = require('csswring');
 var cssnext = require('cssnext');
+var precss = require('precss');
 var autoprefixer = require('autoprefixer-core');
 
 gulp.task('styles', function() {
   var processors = [
     // csswring,
+    precss({}),
     autoprefixer({browsers:['last 2 version']}),
     cssnext({})
   ];
